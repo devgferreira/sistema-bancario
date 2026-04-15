6224fed7a2527162228f990f9352048d90169bdc
gabriel <gabrielferreira.com87@gmail.com>
2026-04-14 21:38:51 -0300

    feat: criando a camada de dominio

---
+++ SistemaBancario/SistemaBancario.Domain/Entidades/Users/UsersInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaBancario.Domain.Entidades.Users
+{
+    public class UsersInfo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ SistemaBancario/SistemaBancario.Domain/Interfaces/Users/IUsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using SistemaBancario.Domain.Entidades.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaBancario.Domain.Interfaces.Users
+{
+    public interface IUsersRepository
+    {
+        Task CriarUsuario(UsersInfo usersInfo);
+        Task<List<UsersInfo>> BuscarUsuario(int id, string email, string cpf);
+        Task AtualizarUsuario(int id, UsersInfo usersInfo);
+        Task DeletarUsuario(int id);
+    }
+}
