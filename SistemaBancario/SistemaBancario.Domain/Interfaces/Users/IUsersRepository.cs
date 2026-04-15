256f5d63f53dd801f1db14d8dfd3dd77c07f9256
gabriel <gabrielferreira.com87@gmail.com>
2026-04-14 22:21:02 -0300

    feat: implementando o repositorio do user

--- SistemaBancario/SistemaBancario.Domain/Interfaces/Users/IUsersRepository.cs
+++ SistemaBancario/SistemaBancario.Domain/Interfaces/Users/IUsersRepository.cs
@@ -8,7 +8,7 @@ namespace SistemaBancario.Domain.Interfaces.Users
     public interface IUsersRepository
     {
         Task CriarUsuario(UsersInfo usersInfo);
-        Task<List<UsersInfo>> BuscarUsuario(int id, string email, string cpf);
+        Task<List<UsersInfo>> BuscarUsuario(int? id, string? email, string? cpf);
         Task AtualizarUsuario(int id, UsersInfo usersInfo);
         Task DeletarUsuario(int id);
     }
---
+++ SistemaBancario/SistemaBancario.Infrastructure/Repository/Users/UsersRepository.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using SistemaBancario.Domain.Entidades.Users;
+using SistemaBancario.Domain.Interfaces.Users;
+using SistemaBancario.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaBancario.Infrastructure.Repository.Users
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private DbContext _context;
+
+        public UsersRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AtualizarUsuario(int id, UsersInfo usersInfo)
+        {
+            var sql = @"UPDATE Users SET Nome = @Nome, Cpf = @Cpf, Email = @Email WHERE Id = @Id";
+            await _context.Connection.ExecuteAsync(sql, new { Nome = usersInfo.Nome, Cpf = usersInfo.Cpf, Email = usersInfo.Email, Id = id });
+        }
+
+        public async Task<List<UsersInfo>> BuscarUsuario(int? id, string? email, string? cpf)
+        {
+            var sql =  @"SELECT ID, CPF, NOME, EMAIL FROM Users WHERE 1 = 1 ";
+            
+            if (id != null)
+            {
+                sql += " AND Id = @Id ";
+            }
+            if (email != null) {
+                sql += " AND Email = @Email ";
+            }
+            if (cpf != null)
+            {
+                sql += " AND Cpf = @Cpf ";
+            }
+            var result = await _context.Connection.QueryAsync<UsersInfo>(sql, new { Id = id, Email = email, Cpf = cpf });
+            return result.ToList();
+        }
+
+        public async Task CriarUsuario(UsersInfo usersInfo)
+        {
+            var sql = @"INSERT INTO Users (Nome, Cpf, Email, Senha) VALUES (@Nome, @Cpf, @Email, @Senha)";
+
+            await _context.Connection.ExecuteAsync(sql,usersInfo);
+        }
+
+        public async Task DeletarUsuario(int id)
+        {
+            var sql = @"DELETE FROM Users WHERE Id = @Id";
+
+            await _context.Connection.ExecuteAsync(sql, new { Id = id });
+        }
+    }
+}
