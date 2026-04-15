18b8a8b8426ff9444522631c703ff91f03beef67
gabriel <gabrielferreira.com87@gmail.com>
2026-04-14 21:47:10 -0300

    feat: criando o context do bd

---
+++ SistemaBancario/SistemaBancario.Infrastructure/Context/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SistemaBancario.Infrastructure.Context
+{
+    public class DbContext : IDisposable
+    {
+        public IDbConnection Connection { get; set; }
+        public DbContext(IConfiguration configuration)
+        {
+            var connStr = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+                  ?? configuration.GetConnectionString("DefaultConnection");
+
+            Connection = new NpgsqlConnection(connStr);
+            Connection.Open();
+        }
+        public void Dispose() => Connection?.Dispose();
+    }
+}
