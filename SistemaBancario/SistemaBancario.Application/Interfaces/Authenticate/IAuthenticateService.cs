e34c03f1ad57e905bdc501357bf98a32e98e9dd2
gabriel <gabrielferreira.com87@gmail.com>
2026-04-15 21:52:56 -0300

    feat: criando a interface :IAuthenticateService

---
+++ SistemaBancario/SistemaBancario.Application/Interfaces/Authenticate/IAuthenticateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaBancario.Application.Interfaces.Authenticate
+{
+    public interface IAuthenticateService
+    {
+        Task<bool> AutenticarAsync(string email, string senha);
+        public string GerarToken(string nome, string email, string cpf);
+    }
+}
