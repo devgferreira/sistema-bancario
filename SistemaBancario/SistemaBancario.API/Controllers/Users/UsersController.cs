cfd283a77e1dec276fd4b32e31b957d03d4ea473
gabriel <gabrielferreira.com87@gmail.com>
2026-04-21 22:19:57 -0300

    feat: criando a controller

---
+++ SistemaBancario/SistemaBancario.API/Controllers/Users/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaBancario.Application.DTO.Users;
+using SistemaBancario.Application.Interfaces.Users;
+
+namespace SistemaBancario.API.Controllers.Users
+{
+    public class UsersController : ControllerBase
+    {
+        private readonly IUsersService _usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] UsersLoginRequest request)
+        {
+            try
+            {
+                var tokenResponse = await _usersService.Login(request);
+                return Ok(tokenResponse);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Registro([FromBody] UsersRegistroRequest request)
+        {
+            try
+            {
+                var tokenResponse = await _usersService.Registro(request);
+                return Ok(tokenResponse);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
