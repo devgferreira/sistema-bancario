5d14d6283a778d6cab2f84dcae4d9b86cb7c21dc
gabriel <gabrielferreira.com87@gmail.com>
2026-04-28 21:01:33 -0300

    feat: criando a service da conta

---
+++ SistemaBancario/SistemaBancario.Application/Interfaces/Conta/IContaService.cs
@@ -0,0 +1,15 @@
+﻿using SistemaBancario.Application.DTO.Conta;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaBancario.Application.Interfaces.Conta
+{
+    public interface IContaService
+    {
+        Task CriarConta(CriarContaDTO criarContaDTO);
+        Task AtualizarConta(AtualizarContaDTO atualizarContaDTO, Guid id, int userId);
+        Task DeletarConta(Guid id, int userId);
+        Task<List<ContaDTO>> BuscarContas(int userId);
+    }
+}
